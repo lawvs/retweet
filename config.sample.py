4257f7335ebcad169872f3fb390f243e82757af1
lawvs <lawvs@users.noreply.github.com>
2018-11-11 23:24:32 +0800

    feat: add main

---
+++ config.sample.py
@@ -0,0 +1,79 @@
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    # 监控账号 Twitter screen name  *Required
+    SCREEN_NAME = 'KanColle_STAFF'
+    # Twitter proxy
+    PROXY = None
+    # 监控间隔  *Required
+    INTERVAL = 300  # second
+
+    # Twitter
+    # https://developer.twitter.com/en/docs/basics/authentication/overview/oauth
+    # OPTIONAL - (TWITTER_API_KEY + TWITTER_API_SECRET) or TWITTER_BEARER_TOKEN
+    # Consumer Key
+    TWITTER_API_KEY = None
+    # Consumer Secret
+    TWITTER_API_SECRET = None
+    TWITTER_BEARER_TOKEN = None
+
+    # Weibo
+    # http://open.weibo.com/wiki/
+    # App ID  *Required
+    WEIBO_APP_ID = '11111'
+    # App Secret  *Required
+    WEIBO_APP_SECRET = 'qwerty'
+    # 应用地址  *Required
+    WEIBO_REDIRECT_URI = 'https://www.example.com'
+    # OPTIONAL - WEIBO_ACCESS_TOKEN or (WEIBO_USERNAME + WEIBO_PASSWORD)
+    WEIBO_ACCESS_TOKEN = None  # access token
+    WEIBO_USERNAME = None
+    WEIBO_PASSWORD = None
+    WEIBO_PREFIX = ''  # 转发微博的前缀 *Optional
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProductionConfig(Config):
+    '''
+    生产环境配置
+    '''
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    '''
+    开发环境配置
+    '''
+    DEBUG = True
+    PROXY = 'https://localhost:1080/'
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+def loadEnv():
+    import os
+    env = os.environ.get('ENV')
+    if env:
+        return env
+    if '.prod' in os.listdir():
+        env = 'prod'
+    else:
+        env = 'default'
+    return env
+
+
+configDict = {
+    'dev': DevelopmentConfig,
+    'test': TestingConfig,
+    'prod': ProductionConfig,
+    'default': DevelopmentConfig
+}
+
+env = loadEnv()
+config = configDict[env]
---
+++ main.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import logging
+from config import config
+from weibo import Client as WeiboClient
+import tweet
+import atexit
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - [%(levelname)s] %(message)s",
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+if sys.version_info[0] < 3:
+    logger.warn("NOTICE: You python version is lower than 3!")
+
+if config.DEBUG:
+    logging.getLogger('root').setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+logger.info('Loaded config: {}'.format(str(config)))
+
+
+weiboClient = None
+tweetClient = None
+
+
+def exit_handler():
+    logger.warn('Application is ending!')
+
+
+def getWeiboClient(config):
+    if config.WEIBO_ACCESS_TOKEN:
+        weiboClient = WeiboClient(config.WEIBO_APP_ID, config.WEIBO_APP_SECRET, config.WEIBO_REDIRECT_URI, token={
+                                  'access_token': config.WEIBO_ACCESS_TOKEN})
+    elif config.WEIBO_USERNAME and config.WEIBO_PASSWORD:
+        weiboClient = WeiboClient(config.WEIBO_APP_ID, config.WEIBO_APP_SECRET, config.WEIBO_REDIRECT_URI,
+                                  username=config.WEIBO_USERNAME, password=config.WEIBO_PASSWORD)
+    else:
+        raise('Get weiboClient fail - WEIBO_ACCESS_TOKEN not found OR (WEIBO_USERNAME and config.WEIBO_PASSWORD) not found')
+    return weiboClient
+
+
+def getTweetClient(config):
+    payload = {
+        'apiKey': config.TWITTER_API_KEY,
+        'apiSecret': config.TWITTER_API_SECRET,
+        'bearerToken': config.TWITTER_BEARER_TOKEN,
+        'proxy': config.PROXY,
+        'screenName': config.SCREEN_NAME,
+        'interval': config.INTERVAL
+    }
+    tweetClient = tweet.Tweet(**payload)
+    return tweetClient
+
+
+def init():
+    global weiboClient, tweetClient, config
+    weiboClient = getWeiboClient(config)
+    tweetClient = getTweetClient(config)
+    logger.info('Monitoring {}'.format(tweetClient.screenName))
+
+
+def postWeibo(text, pic=None):
+    status = '{prefix}{text}{redirect}'.format(
+        prefix=config.WEIBO_PREFIX, text=text, redirect=config.WEIBO_REDIRECT_URI)  # duplicate
+    if len(status) > 140:
+        logger.debug('input text more than 140 characters ' + str(len(text)))
+        text = text[0:(140 - len(status) - 3)] + '...'
+        status = '{prefix}{text}{redirect}'.format(
+            prefix=config.WEIBO_PREFIX, text=text, redirect=config.WEIBO_REDIRECT_URI)  # duplicate
+    logger.info('share weibo... - {}'.format(status))
+    resp = weiboClient.shareWeibo(status, pic)
+    logger.debug('shareWeibo resp - ' + str(resp))
+
+
+def filterTweet(l):
+    # filter photo
+    # l = filter(tweet.hasImage, l)  # tweet contains photo
+    l = list(l)
+    return l
+
+
+def loop():
+    l = tweetClient.getNewTweets()
+    if len(l) <= 0:
+        return
+
+    for tweet in l:
+        url = 'https://twitter.com/{}/status/{}'.format(
+            tweetClient.screenName, tweet.get('id'))
+        logger.info('new tweet - {}'.format(url))
+
+    l = filterTweet(l)
+    l = map(tweet.mapPhotoList, l)  # simplify obj
+    l = list(l)
+    if len(l) <= 0:
+        return
+
+    for d in l:
+        text = d.get('full_text')
+        photoList = d.get('photoList')
+        if len(photoList) <= 0:
+            return
+
+        url = 'https://twitter.com/{}/status/{}'.format(
+            tweetClient.screenName, d.get('id'))
+        # logger.info('photo tweet -> {}'.format(url))
+        if len(photoList) > 1:
+            logging.warn('tweet 图片数量超过一张 -> {}'.format(url))
+        pic = tweet.getPhoto(photoList[0], proxy=config.PROXY)
+        postWeibo(text, pic)
+
+
+def main():
+    init()
+
+    if '-f' in sys.argv:  # debug argv
+        loop()
+    if config.INTERVAL < 30:
+        logger.warn('sleep time is too short! sleepTime: {}s. Forced to change to 30s'.format(
+            config.INTERVAL))
+        config.INTERVAL = 30
+
+    while True:
+        logger.debug('sleep {}s...'.format(config.INTERVAL))
+        time.sleep(config.INTERVAL)
+        loop()
+
+
+if __name__ == '__main__':
+    atexit.register(exit_handler)
+    main()
